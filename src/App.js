85d57e9f7f7c05b8f0762eae4f63b29ee5310c34
ALI-ZIA -KHAN <azk443.443@gmail.com>
2023-05-31 10:33:03 +0500

    mongo eliminated

--- src/App.js
+++ src/App.js
@@ -3,43 +3,43 @@ import './App.css';
 
 function App() {
 
-  const { MongoClient } = require('mongodb');
+  // const { MongoClient } = require('mongodb');
 
-  async function connectToMongoDB() {
-    try {
-      const uri = 'mongodb+srv://<username>:<password>@<cluster-url>/<database>?retryWrites=true&w=majority';
-      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  // async function connectToMongoDB() {
+  //   try {
+  //     const uri = 'mongodb+srv://<username>:<password>@<cluster-url>/<database>?retryWrites=true&w=majority';
+  //     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-      await client.connect();
-      console.log('Connected to MongoDB Atlas');
+  //     await client.connect();
+  //     console.log('Connected to MongoDB Atlas');
 
-      const db = client.db('<database-name>');
-      // Perform operations on the database
+  //     const db = client.db('<database-name>');
+  //     // Perform operations on the database
 
-      // Example: Insert a document
-      const collection = db.collection('<collection-name>');
-      await collection.insertOne({ name: 'John Doe', age: 30 });
-      console.log('Document inserted successfully');
+  //     // Example: Insert a document
+  //     const collection = db.collection('<collection-name>');
+  //     await collection.insertOne({ name: 'John Doe', age: 30 });
+  //     console.log('Document inserted successfully');
 
-      // Example: Query documents
-      const query = { age: { $gt: 25 } };
-      const result = await collection.find(query).toArray();
-      console.log('Query results:', result);
+  //     // Example: Query documents
+  //     const query = { age: { $gt: 25 } };
+  //     const result = await collection.find(query).toArray();
+  //     console.log('Query results:', result);
 
-      // Close the connection
-      await client.close();
-      console.log('Disconnected from MongoDB Atlas');
-    } catch (error) {
-      console.error('Error connecting to MongoDB Atlas:', error);
-    }
-  }
+  //     // Close the connection
+  //     await client.close();
+  //     console.log('Disconnected from MongoDB Atlas');
+  //   } catch (error) {
+  //     console.error('Error connecting to MongoDB Atlas:', error);
+  //   }
+  // }
 
-  // Call the function to establish the connection
-  connectToMongoDB();
+  // // Call the function to establish the connection
+  // connectToMongoDB();
 
-  useEffect(() => {
-    connectToMongoDB()
-  }, [])
+  // useEffect(() => {
+  //   connectToMongoDB()
+  // }, [])
   return (
     <div className="App">
       <header className="App-header">
